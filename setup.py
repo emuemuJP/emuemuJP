dfaf08eea2a2e4e823ab248f3af7f8da81c1b15d
emuemuJP <k.matsumoto.0807@gmail.com>
2022-09-02 18:39:02 +0900

    0.0.3

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+requires = ["twine>=3.8.0",
+            "wheel>=0.37.1"]
+
 setup(
     name='emuemuJP',
-    version='0.0.1',
+    version='0.0.3',
     packages=find_packages(),
 
     author='emuemuJP',
@@ -13,7 +16,7 @@ setup(
     long_description_content_type='text/markdown',
 
     python_requires='~=3.6',
-
+    install_requires=requires,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
